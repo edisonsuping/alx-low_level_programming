bd39a92d8f08a8a592e9ffc7aeea4f6dc16080f3
Mista Edison <mistaedison@yahoo.com>
2023-02-16 14:41:55 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
-
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - A program that prints a line with puts function
+ * Return: 0(Success)
  */
-int main"\"Programming is like buliding a multilingual puzzle)
-{
+int main(void)
+{	
+	puts("\"Programming is like buliding a multilingual puzzle");
     return (0);
 }
