f3fc724e52dc0a8220cb9fa4edc7d68076008182
Mista Edison <mistaedison@yahoo.com>
2023-02-16 16:14:07 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+*main - A program that prints the size of various computer types
+*Return:0(success)
+*/
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+printf("size of a char:%lu byte(s)\n",(unsigned long)size of(a));
+printf("size of an int:%lu bytes(s)\n",(unsigned long)size of(b));
+printf("size of a long int:%lu byte(s)\n",(unsigned long )size of(c));
+printf("size of a long long int:%lu byte(s)\n",(unsigned long)size of (d));
+printf("size of a float:%lu byte(s)\n",(unsigned long)size of (f));
+Return (0);
+}
