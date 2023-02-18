29939e9d8c9616abed696a9f313bdd958af3c7dd
Mista Edison <mistaedison@yahoo.com>
2023-02-18 09:48:43 -0800

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -23,9 +23,9 @@ int main(void)
 	{
 		printf("Last digit of %d is %d and is 0\n", n, a);
 	}
-	else (n < 6)
+	else
 	{
-		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
+		printf("Last digit of %d is %d and is less than 6 and is not 0\n", n, a);
 	}
 	return (0);
 }
