51931b8200894c5aa97180a9bbedac76ee04ca86
Mista Edison <mistaedison@yahoo.com>
2023-02-19 00:09:33 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,3 +1,17 @@
 #include <stdio.h>
 
+/**
+ * main - prints alpha in lower case
+ * Return: Always (0) (success)
+ */
+int main(void)
+{
+	char lc;
 
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
+}
