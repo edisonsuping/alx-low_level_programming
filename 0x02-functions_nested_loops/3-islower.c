89839d3829d39caf1c78bf914440b60aa588a96e
Mista Edison <mistaedison@yahoo.com>
2023-02-22 07:19:49 -0800

    3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -3,6 +3,8 @@
 /**
  * main -_islower - Checks for lowercase character
  * @c: The character to be checked
+ * Description: converts the uppercase letter C to the
+ * corresponding lowercase letter
  * Return: 1 for the lowercase character or 0 for anything else
  */
 int _islower(int c)
