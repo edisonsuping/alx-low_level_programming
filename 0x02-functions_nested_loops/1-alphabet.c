3edd06d7e256021d815a8aec216317d2325235aa
Mista Edison <mistaedison@yahoo.com>
2023-02-21 16:09:08 -0800

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -4,13 +4,14 @@
  * main - a function that prints the alphabet in lowercase
  * Return: Always (0) (success)
  */
-int main(void)
+void print_alphabet(void)
 {
 	char c;
 
 	for (c = 'a'; c <= 'z'; c++)
+	{
 		_putchar(c);
+	}
 
 	_putchar('\n');
-	return (0);
 }
